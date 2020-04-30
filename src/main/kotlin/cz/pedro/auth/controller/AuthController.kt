227d6d6382ed0596a7af3ca5673bbf62f8f24795
Petr Kadlec <kadlec@2n.cz>
2020-04-30 21:03:12 +0200

    auth controller & service initial commit

--- src/main/kotlin/cz/pedro/auth/controller/AuthController.kt
+++ src/main/kotlin/cz/pedro/auth/controller/AuthController.kt
@@ -1,16 +1,18 @@
 package cz.pedro.auth.controller
 
+import cz.pedro.auth.service.AuthService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/auth"])
-class AuthController {
+class AuthController(@Autowired val authService: AuthService) {
 
     @PostMapping(path = ["/login"])
     fun auth() {
-        TODO()
+        authService.login()
     }
 
 }
---
+++ src/main/kotlin/cz/pedro/auth/service/AuthService.kt
@@ -0,0 +1,12 @@
+package cz.pedro.auth.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService {
+
+    fun login() {
+        TODO()
+    }
+
+}
