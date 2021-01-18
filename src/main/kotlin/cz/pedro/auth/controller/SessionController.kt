8a72d84271677454861144f28be0d1a54e060fd7
pedro-cze <petrkadlec.net@gmail.com>
2021-01-18 21:48:36 +0100

    ktlint update

--- src/main/kotlin/cz/pedro/auth/controller/SessionController.kt
+++ src/main/kotlin/cz/pedro/auth/controller/SessionController.kt
@@ -11,7 +11,11 @@ import cz.pedro.auth.util.Either.Left
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 @RequestMapping(path = ["/session"])
